6310a1a03391d3e5121e48ffa0b36acb0bec9cae
misshanya <mishanyadev@gmail.com>
2025-02-23 00:12:50 +0300

    add logging and time handling

--- internal/handlers/ollama.go
+++ internal/handlers/ollama.go
@@ -3,6 +3,8 @@ package handlers
 import (
 	"context"
 	"fmt"
+	"log"
+	"time"
 
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
@@ -20,16 +22,25 @@ func NewOllamaHandler(service *services.OllamaService) *OllamaHandler {
 }
 
 func (h *OllamaHandler) SendMessage(ctx context.Context, b *bot.Bot, update *models.Update) {
+	startTime := time.Now()
 	response, err := h.service.SendMessage(ctx, update.Message.Text)
 	if err != nil {
+		log.Printf("Failed to generate: %v", err)
 		b.SendMessage(ctx, &bot.SendMessageParams{
 			ChatID: update.Message.Chat.ID,
 			Text:   "Произошла ошибка при генерации :(",
 		})
 		return
 	}
-	b.SendMessage(ctx, &bot.SendMessageParams{
+	endTime := time.Now()
+
+	timeDiff := endTime.Unix() - startTime.Unix()
+	_, err = b.SendMessage(ctx, &bot.SendMessageParams{
 		ChatID: update.Message.Chat.ID,
-		Text:   fmt.Sprintf("Ответ нейросети:\n%s", response),
+		Text:   fmt.Sprintf("Ответ нейросети занял %v сек:\n%s", timeDiff, response),
 	})
+	if err != nil {
+		log.Printf("Failed to send message: %v", err)
+	}
+	log.Printf("Message from %s: %s;\nAI response: %s", update.Message.Chat.FirstName, update.Message.Text, response)
 }
